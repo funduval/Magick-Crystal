891f9c68ed7d6b85e2d5de644b55a2903130e3c7
funduval <fundaduval@gmail.com>
2017-05-08 09:23:30 -0400

    getting rid of ugly alerts and adding dynamic css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,12 @@
-//declare universal variablés
+//declare universal variablés for win/lose logic
 
 
 var win = 0;
 var lose = 0;
 var yourTotal = 0;
 
+
+//declare universal variablés for random numbers generated
 
 var mysteryNumber;
 var magenta;
@@ -46,6 +48,8 @@ function gameInit() {
 
         $("#totalsAdded").html(yourTotal);
 
+        console.log(mysteryNumber);
+
         var numReveal = $("<h2>");
 
         numReveal.addClass("numero");
@@ -71,6 +75,7 @@ function gameInit() {
         yourTotal = yourTotal + blue;
         $("#totalsAdded").html(yourTotal);
         winLoseTest();
+        console.log(blue);
 
     });
 
@@ -79,6 +84,7 @@ function gameInit() {
         yourTotal = yourTotal + yellow;
         $("#totalsAdded").html(yourTotal);
         winLoseTest();
+        console.log(yellow);
 
     });
 
@@ -87,6 +93,7 @@ function gameInit() {
         yourTotal = yourTotal + magenta;
         $("#totalsAdded").html(yourTotal);
         winLoseTest();
+        console.log(magenta);
 
     });
 
@@ -95,41 +102,66 @@ function gameInit() {
         yourTotal = yourTotal + orange;
         $("#totalsAdded").html(yourTotal);
         winLoseTest();
+        console.log(orange);
 
     });
 
-    //FUNCTION JUNCTION
+    //UNIVERSAL FUNCTION JUNCTION
 
     function winLoseTest() {
 
         if (yourTotal === mysteryNumber) {
             win++;
-            $("#wins").html(win);
+            console.log(win);
+            console.log(yourTotal);
 
-            alert("You made " + yourTotal + " exactly! You win the round!");
-            $("#totalsAdded").html(yourTotal);
+             $("#wins").html(win);
 
-            reset();
+             $("#totalHead").html("");
+
+            
+
+             $("#totalsAdded").html("You made " + yourTotal + " exactly! <br> <br> You win the round!<hr>");
+             $("#totalsAdded").css("font-size", "28px");
+             $("#totalsAdded").css("height", "200px");
+             $("#totalsAdded").css("height", "400px");
+             $("#totalsAdded").css("background-color", "white");
+             $("#winBox").css("border", "none");
+             $("#totalsBox").css("border", "none");
+             $("#winBox").css("color", "white");
+             $("#wins").css("color", "white");
+             $("#losses").css("color", "white");
+
             setTimeout(function(){
        								 reset();
       					}, 2300);
-            // ==========the reset timeout keeps stopping the audio=====
+            // ==========the reset timeout keeps stopping the audio?=====
 
         };
 
         if (yourTotal > mysteryNumber) {
             lose++;
-            $("#losses").html(lose);
+            console.log(lose);
+            console.log(yourTotal);
 
-            alert("Sorry, you went over " + mysteryNumber + "," + " so you lose!");
-            $("#totalsAdded").html(yourTotal);
+             $("#losses").html(lose);
 
-           
+             $("#totalHead").html("");
+
+             $("#totalsAdded").html("Sorry, you went over " + mysteryNumber + "<br>You got " + yourTotal + "<br><br>You lose this round!<hr>");
+             $("#totalsAdded").css("font-size", "28px", "bold");
+             $("#totalsAdded").css("height", "400px");
+             $("#totalsAdded").css("background-color", "white");
+             $("#winBox").css("border", "none");
+             $("#totalsBox").css("border", "none");
+
+              $("#winBox").css("color", "white");
+              $("#losses").css("color", "white");
 
             setTimeout(function(){
        								 reset();
       					}, 2300); 
-            // ==========the reset timeout keeps stopping the audio======
+            // ==========the reset timeout keeps stopping the audio?======
 
         };
 
@@ -138,11 +170,35 @@ function gameInit() {
 
             yourTotal = 0;
 
+
+            $("#totalHead").html("YOUR TOTAL:");
             $("#totalsAdded").html(yourTotal);
+
+            // css to make the invisible fonts visible again
+
+            $("#winBox").css("color", "grey");
+            $("#wins").css("color", "grey");
+            $("#losses").css("color", "grey");
+
+
+             // css to restore previous borders & othere attributes
+
+            $("#winBox").css("border-style", "solid");
+            $("#totalsBox").css("border-style", "solid");
+            $("#winBox").css("border-color", "white");
+            $("#totalsBox").css("border-color", "white");
+            $("#winBox").css("border-size", "white");
+            $("#totalsBox").css("border-size", "white");
+
+            $("#totalsAdded").css("font-size", "50px");
+            $("#totalsAdded").css("height", "200px");
+            $("#totalsAdded").css("background", "none");
+
             $(".clickCommand").html("Click the Magic <br> Crystal");
             $(".clickCommand").css("font-size", "30px");
             $(".clickCommand").css("color", "white");
             $("#texty").html("Try to Add<br>Crystals Up To <br> This New Number:");
+            $("#texty").css("font-family", "Inknut Antiqua", "serif");
 
             mysteryNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
             magenta = Math.floor(Math.random() * 11 + 1);
